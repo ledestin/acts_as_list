52a6f6275c6019e784a0e34e9c1484156bd8b2b5
Dmitry Maksyoma <ledestin@gmail.com>
2016-09-06 21:31:09 +1200

    Refactoring: extract ColumnDefiner

--- lib/acts_as_list/active_record/acts/list.rb
+++ lib/acts_as_list/active_record/acts/list.rb
@@ -1,3 +1,4 @@
+require_relative "column_definer"
 require_relative "scope_definer"
 
 module ActiveRecord
@@ -40,6 +41,7 @@ module ActiveRecord
           caller_class = self
 
           ScopeDefiner.call(caller_class, scope)
+          ColumnDefiner.call(caller_class, column)
 
           class_eval do
             define_singleton_method :acts_as_list_top do
@@ -54,31 +56,8 @@ module ActiveRecord
               caller_class
             end
 
-            define_method :position_column do
-              column
-            end
-
             define_method :add_new_at do
               add_new_at
-            end
-
-            define_method :"#{column}=" do |position|
-              write_attribute(column, position)
-              @position_changed = true
-            end
-
-            # only add to attr_accessible
-            # if the class has some mass_assignment_protection
-            if defined?(accessible_attributes) and !accessible_attributes.blank?
-              attr_accessible :"#{column}"
-            end
-
-            define_singleton_method :quoted_position_column do
-              @_quoted_position_column ||= connection.quote_column_name(column)
-            end
-
-            define_singleton_method :quoted_position_column_with_table_name do
-              @_quoted_position_column_with_table_name ||= "#{caller_class.quoted_table_name}.#{quoted_position_column}"
             end
 
             self.scope :in_list, lambda { where("#{quoted_position_column_with_table_name} IS NOT NULL") }
