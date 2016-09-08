49193dfb143ac0282296e33a5378ca87c48ed9d6
Dmitry Maksyoma <ledestin@gmail.com>
2016-09-08 13:39:49 +1200

    Refactor

--- lib/acts_as_list/active_record/acts/scope_definer.rb
+++ lib/acts_as_list/active_record/acts/scope_definer.rb
@@ -1,53 +1,49 @@
-module ActiveRecord
-  module Acts #:nodoc:
-    module ScopeDefiner #:nodoc:
-      extend ActiveSupport::Inflector
+module ActiveRecord::Acts::ScopeDefiner #:nodoc:
+  extend ActiveSupport::Inflector
 
-      def self.call(caller_class, scope)
-        scope = idify(scope) if scope.is_a?(Symbol)
+  def self.call(caller_class, scope)
+    scope = idify(scope) if scope.is_a?(Symbol)
 
-        caller_class.class_eval do
-          self.scope :in_list, lambda { where("#{quoted_position_column_with_table_name} IS NOT NULL") }
+    caller_class.class_eval do
+      self.scope :in_list, lambda { where("#{quoted_position_column_with_table_name} IS NOT NULL") }
 
-          define_method :scope_name do
-            scope
-          end
+      define_method :scope_name do
+        scope
+      end
 
-          if scope.is_a?(Symbol)
-            define_method :scope_condition do
-              { scope => send(scope) }
-            end
+      if scope.is_a?(Symbol)
+        define_method :scope_condition do
+          { scope => send(scope) }
+        end
 
-            define_method :scope_changed? do
-              changed.include?(scope_name.to_s)
-            end
-          elsif scope.is_a?(Array)
-            define_method :scope_condition do
-              scope.inject({}) do |hash, column|
-                hash.merge!({ column.to_sym => read_attribute(column.to_sym) })
-              end
-            end
-
-            define_method :scope_changed? do
-              (scope_condition.keys & changed.map(&:to_sym)).any?
-            end
-          else
-            define_method :scope_condition do
-              eval "%{#{scope}}"
-            end
-
-            define_method :scope_changed? do
-              false
-            end
+        define_method :scope_changed? do
+          changed.include?(scope_name.to_s)
+        end
+      elsif scope.is_a?(Array)
+        define_method :scope_condition do
+          scope.inject({}) do |hash, column|
+            hash.merge!({ column.to_sym => read_attribute(column.to_sym) })
           end
         end
-      end
 
-      def self.idify(name)
-        return name if name.to_s =~ /_id$/
+        define_method :scope_changed? do
+          (scope_condition.keys & changed.map(&:to_sym)).any?
+        end
+      else
+        define_method :scope_condition do
+          eval "%{#{scope}}"
+        end
 
-        foreign_key(name).to_sym
+        define_method :scope_changed? do
+          false
+        end
       end
     end
+  end
+
+  def self.idify(name)
+    return name if name.to_s =~ /_id$/
+
+    foreign_key(name).to_sym
   end
 end
